1ab7f9dcaba8289faac25f3437f89be1da78c02d
GuillaumePressiat <guillaume.pressiat@aphp.fr>
2020-09-26 13:30:34 +0200

    Ajout plot par classe d'âge

--- app.R
+++ app.R
@@ -125,6 +125,21 @@ data_and_meta <- unique(names(liste_url)) %>%
 lib_dep <- readr::read_csv('cog/departement2019.csv')
 lib_reg <- readr::read_csv('cog/region2019.csv')
 
+to_plot_clage <- data_and_meta %>% purrr::map('incidence') %>% 
+  purrr::compact() %>% 
+  .[[1]] %>% 
+  .$donnee %>% 
+  filter(cl_age90 != '0') %>% 
+  group_by(semaine, jour, dep, cl_age90, pop) %>% 
+  summarise(P = sum(P)) %>% 
+  mutate(inc = P * 1e5 / pop) %>% 
+  ungroup() %>% 
+  left_join(lib_dep %>% rename(libelle_dep = libelle)) %>% 
+  left_join(lib_reg %>% select(reg, libelle_reg = libelle)) %>% 
+  filter(!is.na(libelle_dep)) %>% 
+  mutate(dep = paste0(dep, " - ", libelle_dep),
+         reg = paste0(reg, " - ", libelle_reg))
+
 to_plot <- data_and_meta %>% purrr::map('incidence') %>% 
   purrr::compact() %>% 
   .[[1]] %>% 
@@ -158,11 +173,45 @@ to_plot_jour <- data_and_meta %>% purrr::map('incidence') %>%
   mutate(dep = paste0(dep, " - ", libelle_dep),
          reg = paste0(reg, " - ", libelle_reg))
 
-dataa <- data_and_meta %>% purrr::map('incidence') %>% 
+dataa <- data_and_meta %>% 
+  purrr::map('incidence') %>% 
   purrr::compact() %>% 
   .[[1]] %>% 
   .$donnee %>% as.data.frame
+
+dataa1 <- dataa %>% 
+  left_join(lib_dep %>% rename(libelle_dep = libelle)) %>% 
+  left_join(lib_reg %>% select(reg, libelle_reg = libelle)) %>% 
+  mutate(dep = paste0(dep, " - ", libelle_dep),
+         reg = paste0(reg, " - ", libelle_reg)) %>% 
+  select(-contains('ncc'), - contains('chefl'), - contains('libelle')) %>% 
+  mutate(`Classe d'âge` = case_when(cl_age90 == "09" ~ "0 - 9 ans",
+                                    cl_age90 == "19" ~ "10 - 19 ans",
+                                    cl_age90 == "29" ~ "20 - 29 ans",
+                                    cl_age90 == "39" ~ "30 - 39 ans",
+                                    cl_age90 == "49" ~ "40 - 49 ans",
+                                    cl_age90 == "59" ~ "50 - 59 ans",
+                                    cl_age90 == "69" ~ "60 - 69 ans",
+                                    cl_age90 == "79" ~ "70 - 79 ans",
+                                    cl_age90 == "89" ~ "80 - 89 ans",
+                                    cl_age90 == "90" ~ "90 ans et +",
+                                    TRUE ~ "Toutes classes d'âge"))
+
 library(ggplot2)
+to_plot_clage <- to_plot_clage %>% 
+  mutate(dep = forcats::fct_rev(dep)) %>% 
+  mutate(`Classe d'âge` = case_when(cl_age90 == "09" ~ "0 - 9 ans",
+                              cl_age90 == "19" ~ "10 - 19 ans",
+                              cl_age90 == "29" ~ "20 - 29 ans",
+                              cl_age90 == "39" ~ "30 - 39 ans",
+                              cl_age90 == "49" ~ "40 - 49 ans",
+                              cl_age90 == "59" ~ "50 - 59 ans",
+                              cl_age90 == "69" ~ "60 - 69 ans",
+                              cl_age90 == "79" ~ "70 - 79 ans",
+                              cl_age90 == "89" ~ "80 - 89 ans",
+                              cl_age90 == "90" ~ "90 ans et +"))
+
+# sum(te[te$semaine == "S35",]$pop)
 
 # plotly::ggplotly(ggplot(to_plot) + 
 #   geom_tile(aes(x = semaine, y = dep, fill = P, col = type_semaine)) + 
@@ -191,39 +240,60 @@ liste_departement <- unique(to_plot$dep)
 
 ui <- dashboardPage(skin = "black",
                     
-  dashboardHeader(title = "COVID via SI-DEP"),
-  dashboardSidebar(
-    switchInput(label = "<i class='far fa-calendar'></i>", 'donnee', onLabel = 'Sem.', offLabel = 'Jour', TRUE, size = 'mini'),
-    pickerInput(
-    inputId = "lregs",
-    label = "Régions",
-    choices = sort(unique(to_plot$reg)),
-    options = list(
-      `actions-box` = TRUE), 
-    multiple = TRUE,
-    selected = sort(unique(to_plot$reg))
-  ),
-  pickerInput(
-    inputId = "ldeps",
-    label = "Départements",
-    choices = sort(unique(to_plot$dep)),
-    options = list(
-      `actions-box` = TRUE), 
-    multiple = TRUE,
-    selected = unique(to_plot$dep)
-  ),
-  h5('Dernière mise à jour le : ', br(), data_and_meta[[1]]$incidence$df_date),
-  h5(tags$a(href = 'https://www.data.gouv.fr/fr/datasets/taux-dincidence-de-lepidemie-de-covid-19/', 'Source : Données SI-DEP Santé publique France'))),
-  dashboardBody(
-    verticalLayout(
-      textOutput('value'),
-    box(title = "Effectifs tests positifs", plotly::plotlyOutput("plot", height = "600px", width = "auto"), 
-        collapsible = TRUE, collapsed = FALSE, width = "auto", height = "auto"),
-    box(title = 'Incidence pour 100 000 habitants', plotly::plotlyOutput("ploti", height = "600px", width = "auto"), 
-        collapsible = TRUE,  collapsed = TRUE, width = "auto", height = "auto")),
-    box(title = 'Données sources brutes', DT::dataTableOutput('data'), 
-        collapsible = TRUE,  collapsed = TRUE, width = "auto", height = "auto")
-))
+                    dashboardHeader(title = "COVID via SI-DEP"),
+                    dashboardSidebar(
+                      switchInput(label = "<i class='far fa-calendar'></i>", 'donnee', onLabel = 'Sem.', offLabel = 'Jour', TRUE, size = 'mini'),
+                      switchInput(label = "<i class='far fa-info-circle'></i>", 'incide', onLabel = 'Effectifs', offLabel = 'Incidence', FALSE, size = 'mini'),
+                      pickerInput(
+                        inputId = "lregs",
+                        label = "Régions",
+                        choices = sort(unique(to_plot$reg)),
+                        options = list(
+                          `actions-box` = TRUE), 
+                        multiple = TRUE,
+                        selected = sort(unique(to_plot$reg))
+                      ),
+                      pickerInput(
+                        inputId = "ldeps",
+                        label = "Départements",
+                        choices = sort(unique(to_plot$dep)),
+                        options = list(
+                          `live-search` = TRUE,
+                          # `mobile` = TRUE,
+                          `actions-box` = TRUE), 
+                        multiple = TRUE,
+                        selected = unique(to_plot$dep)
+                      ),
+                      sidebarMenu(
+                        menuItem("Géographie", tabName = "geographie", icon = icon("location-arrow")),
+                        menuItem("Classes d'âge", icon = icon("eye"), tabName = "clages",
+                                 badgeLabel = "new", badgeColor = "green"),
+                        menuItem("Données brutes", icon = icon("th"), tabName = "databr",
+                                 badgeLabel = "new", badgeColor = "green")
+                      ),
+                      br(),
+                      HTML('&nbsp;'), tags$em('   Dernière mise à jour le : ', br(),HTML('&nbsp;'), data_and_meta[[1]]$incidence$df_date),
+                      br(),
+                      tags$b(tags$a(href = 'https://www.data.gouv.fr/fr/datasets/taux-dincidence-de-lepidemie-de-covid-19/', 'Source : Données SI-DEP Santé publique France'))),
+                    dashboardBody(
+                      tags$em(textOutput('value')),
+                      tabItems(
+                        tabItem(tabName = "geographie",
+                                
+                                  h2(textOutput('title')), 
+                                  plotly::plotlyOutput("plot"), height = "650px"),
+                        
+                        tabItem(tabName = "clages",
+                                
+                                  h2(textOutput("title_clage")), 
+                                  plotly::plotlyOutput('plot_clage', height = "650px")),
+                        
+                        tabItem(tabName = "databr",
+                                
+                                  h2('Données sources brutes'), 
+                                  DT::dataTableOutput('data'))))
+                    
+)
 
 server <- function(input, output, session) {
   output$value <- renderText({
@@ -234,47 +304,105 @@ server <- function(input, output, session) {
     }
   })
   
-  plotting <- reactive({
-    if (input$donnee){
-    to_plot %>% 
-      filter(reg %in% input$lregs, dep %in% input$ldeps) %>% 
-        rename(time_ = semaine)
+  output$title <- renderText({
+    if (input$incide){
+      "Effectifs positifs"
     } else {
-      to_plot_jour %>% 
-        filter(reg %in% input$lregs, dep %in% input$ldeps) %>% 
-        rename(time_ = jour)
+      "Incidence pour 100 000 habitants"
     }
   })
   
-
+  output$title_clage <- renderText({
+    if (input$incide){
+      "Effectifs positifs par classe d'âge"
+    } else {
+      "Incidence pour 100 000 habitants par classe d'âge"
+    }
+  })
+  plotting <- reactive({
+    if (input$donnee){
+      temp <- to_plot %>% 
+        filter(reg %in% input$lregs, dep %in% input$ldeps) %>% 
+        rename(time_ = semaine)
+    } else {
+      temp <- to_plot_jour %>% 
+        filter(reg %in% input$lregs, dep %in% input$ldeps) %>% 
+        rename(time_ = jour)
+    }
+    
+    if (input$incide){
+      temp %>% rename(mesure = P)
+    } else {
+      temp %>% rename(mesure = inc)
+    }
+  })
+  
+  plotting_clage <- reactive({
+    if (input$donnee){
+      temp <- to_plot_clage %>% 
+        filter(reg %in% input$lregs, dep %in% input$ldeps) %>% 
+        group_by(semaine, `Classe d'âge`, pop) %>% 
+        summarise(nb_jour = n_distinct(jour),
+                  P = sum(P)) %>% 
+        ungroup() %>% 
+        group_by(semaine, `Classe d'âge`, nb_jour) %>% 
+        summarise(P = sum(P),
+                  pop = sum(pop)) %>% 
+        ungroup() %>% 
+        mutate(inc = P *1e5/pop) %>% 
+        mutate(semaine = ifelse(nb_jour < 7, paste0(semaine, "*\n", nb_jour, "j"), semaine)) %>% 
+        rename(time_ = semaine)
+    } else {
+      temp <- to_plot_clage %>% 
+        filter(reg %in% input$lregs, dep %in% input$ldeps) %>% 
+        group_by(jour, `Classe d'âge`) %>% 
+        summarise(pop = sum(pop),
+                  P = sum(P)) %>% 
+        mutate(inc = P *1e5/pop) %>% 
+        ungroup() %>% 
+        rename(time_ = jour)
+    }
+    
+    if (input$incide){
+      temp %>% rename(mesure = P)
+    } else {
+      temp %>% rename(mesure = inc)
+    }
+  })
+  
+  cdata <- session$clientData
+  
   output$plot <- plotly::renderPlotly(
-    plotly::ggplotly(ggplot(plotting() %>% mutate(dep = forcats::fct_rev(dep))) + 
-                       geom_tile(aes(x = time_, y = dep, fill = P)) + 
+    plotly::ggplotly(ggplot(plotting() %>% mutate(dep = forcats::fct_rev(dep))) +
+                       geom_tile(aes(x = time_, y = dep, fill = mesure)) +
                        scico::scale_fill_scico(begin = 0.2) +
                        guides(colour = "none") +
                        theme_dark() +
-                       labs(x = "", y = ""))
+                       labs(x = "", y = ""), height = 650)
   )
   
-  output$ploti <- plotly::renderPlotly(
-    plotly::ggplotly(ggplot(plotting() %>% mutate(dep = forcats::fct_rev(dep))) + 
-                       geom_tile(aes(x = time_, y = dep, fill = inc)) + 
-                       scico::scale_fill_scico(begin = 0.2) +
-                       guides(colour = "none") +
-                       theme_dark() +
-                       labs(x = "", y = ""))
-  )
   
-  output$data <- DT::renderDataTable(dataa, filter = 'top')
+  output$plot_clage <- plotly::renderPlotly(plotly::ggplotly(ggplot(plotting_clage()) +
+                                                               geom_tile(aes(x = time_, y = `Classe d'âge`, fill = mesure)) +
+                                                               scico::scale_fill_scico(begin = 0.2) +
+                                                               guides(colour = "none") +
+                                                               theme_dark() +
+                                                               labs(x = "", y = ""),  height = 650))
+  
+  output$data <- DT::renderDataTable(dataa1 %>% 
+                                       filter(reg %in% input$lregs, dep %in% input$ldeps), 
+                                     filter = 'top',
+                                     options = list(
+                                      pageLength = 50))
   
   observe({
-      query <- parseQueryString(session$clientData$url_search)
-      if (!is.null(query[['reg']])) {
-          updatePickerInput(session, "lregs", selected = liste_region[grepl(query[['reg']], liste_region)])
-      }
-      if (!is.null(query[['dep']])) {
-          updatePickerInput(session, "ldeps", selected = liste_departement[grepl(query[['dep']], liste_departement)])
-      }
+    query <- parseQueryString(session$clientData$url_search)
+    if (!is.null(query[['reg']])) {
+      updatePickerInput(session, "lregs", selected = liste_region[grepl(query[['reg']], liste_region)])
+    }
+    if (!is.null(query[['dep']])) {
+      updatePickerInput(session, "ldeps", selected = liste_departement[grepl(query[['dep']], liste_departement)])
+    }
   })
   
   observeEvent(input$lregs,{
